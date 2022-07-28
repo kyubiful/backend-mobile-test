dca199518f94905ea38259cbe6db813fcd86a845
Kyubiful <ser.zabala@gmail.com>
2022-07-28 18:55:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const port = process.env.SERVER_PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
