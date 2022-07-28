d7c06b81fcee29f2b8ef35af7f5a563baa90ca7d
Kyubiful <ser.zabala@gmail.com>
2022-07-28 21:30:07 +0200

    feat: create mongodb connection

---
+++ db/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const mongodbUri = process.env.MONGODB_URI
+
+export const mongoDBConnection = () => {
+  mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+      console.log('Mongodb connection error')
+    } else {
+      console.log('Connected to mongodb correctly')
+    }
+  })
+}
