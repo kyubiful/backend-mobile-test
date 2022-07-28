2dbb2770fc71eafe9d75b06642595cebeaf9a736
Kyubiful <ser.zabala@gmail.com>
2022-07-28 21:30:32 +0200

    feat: create mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { mongoDBConnection } from './db/mongodb.js'
 
 const port = process.env.SERVER_PORT || 3000
 
@@ -11,4 +12,5 @@ app.use(cors())
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
+  mongoDBConnection()
 })
