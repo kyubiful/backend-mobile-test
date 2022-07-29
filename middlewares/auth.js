2083f50a4da2ce3392fd3d2e5ebbf91240c4f91d
Kyubiful <ser.zabala@gmail.com>
2022-07-29 15:34:50 +0200

    feat: create auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+import { Users } from '../models/Users.js'
+
+export const isAuthenticated = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (!token) {
+    return res.sendStatus(403)
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (_err, decoded) => {
+    const { id } = decoded
+    Users.findOne({ _id: id }).exec()
+      .then(user => {
+        if (user === null) {
+          return res.sendStatus(403)
+        }
+        req.user = user
+        next()
+      })
+  })
+}
+
+export const isAdmin = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    return res.sendStatus(403)
+  }
+  next()
+}
