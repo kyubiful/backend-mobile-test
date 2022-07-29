822878ada59317b3b1363b83c30bc62972bc3b8f
Kyubiful <ser.zabala@gmail.com>
2022-07-29 15:36:40 +0200

    feat: add new routes and middleware

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import { usersRoutes } from './users.js'
 import { invoicesRoutes } from './invoices.js'
-import { equipmentRoutes } from './equipments.js'
+import { equipmentsRoutes } from './equipments.js'
+import { authorizationsRoutes } from './authorizations.js'
+import { isAuthenticated } from '../middlewares/auth.js'
 
 const app = express()
 
-app.use('/users', usersRoutes)
-app.use('/invoices', invoicesRoutes)
-app.use('/equipments', equipmentRoutes)
+app.use('/users', isAuthenticated, usersRoutes)
+app.use('/invoices', isAuthenticated, invoicesRoutes)
+app.use('/equipments', isAuthenticated, equipmentsRoutes)
+app.use('/', authorizationsRoutes)
 
 export default app
